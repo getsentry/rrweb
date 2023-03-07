78eb9e2fa73d277e14cb9901fdabc8079434f670
Francesco Novy <francesco.novy@sentry.io>
2023-03-07 13:07:41 +0100

    feat: Always mask password inputs

--- packages/rrweb/test/integration.test.ts
+++ packages/rrweb/test/integration.test.ts
@@ -505,11 +505,11 @@ describe('record integration tests', function (this: ISuite) {
       }),
     );
 
-    await page.type('input[type="password"]', 'secr3t');
+    await page.type('#password', 'secr3t');
 
     // Change type to text (simulate "show password")
     await page.click('#show-password');
-    await page.type('input[type="password"]', 'secr3t?');
+    await page.type('#password', 'XY');
     await page.click('#show-password');
 
     const snapshots = await page.evaluate('window.snapshots');
