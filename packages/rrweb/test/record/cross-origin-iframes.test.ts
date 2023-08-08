5e7a5fabf4530644149caeab06e3eebfbc54ce37
Billy Vong <billyvg@users.noreply.github.com>
2023-08-08 17:06:52 -0400

    forgot await

--- packages/rrweb/test/record/cross-origin-iframes.test.ts
+++ packages/rrweb/test/record/cross-origin-iframes.test.ts
@@ -230,7 +230,7 @@ describe('cross origin iframes', function (this: ISuite) {
       await frame.type('textarea', 'textarea test');
       await frame.select('select', '1');
 
-      assertSnapshot(ctx.page);
+      await assertSnapshot(ctx.page);
     });
 
     it('should map scroll events correctly', async () => {
