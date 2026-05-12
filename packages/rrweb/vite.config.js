d242a010a64a961c4724ee7ebeb51d9b2a8658d6
Abdelrahman Awad <abdelrahman.awad@sentry.io>
2026-05-12 10:53:46 -0400

    feat(rrweb): expose canvas manager subpath (#294)
    This PR exposes the `CanvasManager` as a subpath export, the reason for
    this is that the `rrweb` package has some side effects along the export
    path that pins the whole graph.
    
    That prevents Rolldown's DCE from dropping it properly in sentry SDKs,
    blocking the adoption of rolldown. Rollup seemed to handle this
    properly.
    
    I will release this once merged.

--- packages/rrweb/vite.config.js
+++ packages/rrweb/vite.config.js
@@ -1,4 +1,14 @@
 import config from '../../vite.config.default';
 
-// export default config('src/index.ts', 'rrweb', { outputDir: 'dist/main' });
-export default config('src/index.ts', 'rrweb');
+export default config(
+  {
+    rrweb: 'src/index.ts',
+    'canvas-manager': 'src/entries/canvas-manager.ts',
+  },
+  'rrweb',
+  {
+    umdNames: {
+      'canvas-manager': 'rrwebCanvasManager',
+    },
+  },
+);
